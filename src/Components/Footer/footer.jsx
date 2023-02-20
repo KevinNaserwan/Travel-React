b6f3675c23ae726be898e1e677c512995b7cd711
KevinNaserwan <kevinnaserwan@gmail.com>
2023-02-20 23:01:12 +0700

    main

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,11 +1,91 @@
 import React from 'react'
 import './footer.css'
+import video2 from '../../Assets/video2.mp4'
+import {FiSend} from 'react-icons/fi'
+import {MdOutlineTravelExplore} from 'react-icons/md'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiFillInstagram} from 'react-icons/ai'
+import {AiFillYoutube} from 'react-icons/ai'
+import {FaTripadvisor} from 'react-icons/fa'
+import {FiChevronRight} from 'react-icons/fi'
 
 const Footer = () => {
   return (
-    <div>
-      footer
+   <section className='footer'>
+    <div className="videoDiv">
+      <video src={video2} loop autoPlay muted type="video/mp4"></video>
     </div>
+
+    <div className="secContent container">
+      <div className="contactDiv flex">
+        <div className="text">
+          <small>KEEP IN TOUCH</small>
+          <h2>Travel with us</h2>
+        </div>
+
+        <div className="inputDiv flex">
+          <input className="text" placeholder='Enter Email Address'/>
+            <button className='btn flex' type='submit'>
+              SEND <FiSend className='icon'/>
+            </button>
+        </div>
+      </div>
+
+      <div className="footerCard flex">
+        <div className="footerIntro flex">
+          <div className="logoDiv">
+            <a href="#" className='logo flex'>
+              <MdOutlineTravelExplore className="icon"/>Travel.
+            </a>
+          </div>
+
+          <div className="footerParagraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, exercitationem.
+          </div>
+
+          <div className="footerSocials">
+            <AiOutlineTwitter classname="icon"/>
+            <AiFillYoutube className="icon"/>
+            <AiFillInstagram className="icon"/>
+            <FaTripadvisor className='icon'/>
+          </div>
+        </div>
+
+        <div className="footerLinks grid">
+          <div className="linkGroup">
+            <span className="groupTitle">
+              OUR AGENCY
+            </span>
+
+            <li className="footerList">
+              <FiChevronRight className="icon"/>
+              Services
+            </li>
+            <li className="footerList">
+              <FiChevronRight className="icon"/>
+              Services
+            </li>
+            <li className="footerList">
+              <FiChevronRight className="icon"/>
+              Services
+            </li>
+            <li className="footerList">
+              <FiChevronRight className="icon"/>
+              Services
+            </li>
+            <li className="footerList">
+              <FiChevronRight className="icon"/>
+              Services
+            </li>
+            <li className="footerList">
+              <FiChevronRight className="icon"/>
+              Services
+            </li>
+          </div>
+        </div>
+      </div>
+    </div>
+   </section>
   )
 }
 
--- src/Components/main/main.jsx
+++ src/Components/main/main.jsx
@@ -3,6 +3,14 @@ import './mainn.css'
 import {HiOutlineLocationMarker} from 'react-icons/hi'
 import {HiOutlineClipboardCheck} from 'react-icons/hi'
 import img from '../../Assets/img.jpg'
+import img2 from '../../Assets/img2.jpg'
+import img3 from '../../Assets/img3.jpg'
+import img4 from '../../Assets/img4.jpg'
+import img5 from '../../Assets/img5.jpg'
+import img6 from '../../Assets/img6.jpg'
+import img7 from '../../Assets/img7.jpg'
+import img8 from '../../Assets/img8.jpg'
+import img9 from '../../Assets/img9.jpg'
 
 const Data = [
   {
@@ -14,78 +22,78 @@ const Data = [
     fees: '$700',
     description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
   },
-  // {
-  //   id:2,
-  //   imgsrc:img,
-  //   destTitle: 'Bora Bora',
-  //   location: 'New Zealand',
-  //   grade:'CULTURAL RELAX',
-  //   fees: '$700',
-  //   description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
-  // },
-  // {
-  //   id:3,
-  //   imgsrc:img,
-  //   destTitle: 'Bora Bora',
-  //   location: 'New Zealand',
-  //   grade:'CULTURAL RELAX',
-  //   fees: '$700',
-  //   description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
-  // },
-  // {
-  //   id:4,
-  //   imgsrc:img,
-  //   destTitle: 'Bora Bora',
-  //   location: 'New Zealand',
-  //   grade:'CULTURAL RELAX',
-  //   fees: '$700',
-  //   description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
-  // },
-  // {
-  //   id:5,
-  //   imgsrc:img,
-  //   destTitle: 'Bora Bora',
-  //   location: 'New Zealand',
-  //   grade:'CULTURAL RELAX',
-  //   fees: '$700',
-  //   description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
-  // },
-  // {
-  //   id:6,
-  //   imgsrc:img,
-  //   destTitle: 'Bora Bora',
-  //   location: 'New Zealand',
-  //   grade:'CULTURAL RELAX',
-  //   fees: '$700',
-  //   description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
-  // },
-  // {
-  //   id:7,
-  //   imgsrc:img,
-  //   destTitle: 'Bora Bora',
-  //   location: 'New Zealand',
-  //   grade:'CULTURAL RELAX',
-  //   fees: '$700',
-  //   description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
-  // },
-  // {
-  //   id:8,
-  //   imgsrc:img,
-  //   destTitle: 'Bora Bora',
-  //   location: 'New Zealand',
-  //   grade:'CULTURAL RELAX',
-  //   fees: '$700',
-  //   description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
-  // },
-  // {
-  //   id:9,
-  //   imgsrc:img,
-  //   destTitle: 'Bora Bora',
-  //   location: 'New Zealand',
-  //   grade:'CULTURAL RELAX',
-  //   fees: '$700',
-  //   description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
-  // },
+  {
+    id:2,
+    imgsrc:img2,
+    destTitle: 'Machu Pichu',
+    location: 'New Zealand',
+    grade:'CULTURAL RELAX',
+    fees: '$500',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
+  },
+  {
+    id:3,
+    imgsrc:img3,
+    destTitle: 'Eiffel',
+    location: 'Paris',
+    grade:'CULTURAL RELAX',
+    fees: '$900',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
+  },
+  {
+    id:4,
+    imgsrc:img5,
+    destTitle: 'Bali',
+    location: 'Indonesia',
+    grade:'CULTURAL RELAX',
+    fees: '$750',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
+  },
+  {
+    id:5,
+    imgsrc:img4,
+    destTitle: 'Coleseum',
+    location: 'Roma',
+    grade:'CULTURAL RELAX',
+    fees: '$1100',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
+  },
+  {
+    id:6,
+    imgsrc:img6,
+    destTitle: 'London Bridge',
+    location: 'England',
+    grade:'CULTURAL RELAX',
+    fees: '$650',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
+  },
+  {
+    id:7,
+    imgsrc:img7,
+    destTitle: 'Great Wall China',
+    location: 'China',
+    grade:'CULTURAL RELAX',
+    fees: '$400',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
+  },
+  {
+    id:8,
+    imgsrc:img8,
+    destTitle: 'Borobudur',
+    location: 'Indonesia',
+    grade:'CULTURAL RELAX',
+    fees: '$450',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
+  },
+  {
+    id:9,
+    imgsrc:img9,
+    destTitle: 'Cappadocia',
+    location: 'turkey',
+    grade:'CULTURAL RELAX',
+    fees: '$1200',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities. '
+  },
 
 ]
 
@@ -97,37 +105,37 @@ const Main = () => {
         Most visited destinations
       </h3>
     </div>
-    <div className="seeContent grid">
+    <div className="secContent grid">
       {
         Data.map(({id, imgsrc, destTitle, location, grade, fees, description})=> {
           return(
-            <div className="singleDestination">
-              <div className="imagediv">
-                <img src={imgsrc} alt={destTitle} />
-              </div>
-
-              <div className="cardInfo">
-                <h4 className="destTitle">{destTitle}</h4>
-                <span className="continent flex">
-                  <HiOutlineLocationMarker className='icon'/>
-                  <span className="name">{location}</span>
-                </span>
-
-                <div className="fees flex">
-                  <div className="grade">
-                    <span>{grade}<small>+1</small></span>
-                  </div>
-                  <div className="price">
-                    <h5>{fees}</h5>
-                  </div>
+            <div key={id} className="singleDestination">
+                <div className="imagediv">
+                  <img src={imgsrc} alt={destTitle} />
                 </div>
-                <div className="desc">
-                  <p>{description}</p>
+
+                <div className="cardInfo">
+                  <h4 className="destTitle">{destTitle}</h4>
+                  <span className="continent flex">
+                    <HiOutlineLocationMarker className='icon'/>
+                    <span className="name">{location}</span>
+                  </span>
+
+                  <div className="fees flex">
+                    <div className="grade">
+                      <span>{grade}<small>+1</small></span>
+                    </div>
+                    <div className="price">
+                      <h5>{fees}</h5>
+                    </div>
+                  </div>
+                  <div className="desc">
+                    <p>{description}</p>
+                  </div>
+                  <button className='btn flex'>
+                    DETAILS <HiOutlineClipboardCheck className='icon'/>
+                  </button>
                 </div>
-                <button className='btn flex'>
-                  DETAILS <HiOutlineClipboardCheck className='icon'/>
-                </button>
-              </div>
             </div>
           )
         })
